0c66ad17f8786c768abe2d489d9de81e10cfe3b6
unknown <arthurguilherme2013@gmail.com>
2023-06-30 12:51:37 +0100

    End of Html and Sass of the Aplication

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-//51928673cc87f91c0438f7adb5345f32
+//51928673cc87f91c0438f7adb5345f32 
+//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=51928673cc87f91c0438f7adb5345f32
+console.log("Hello")
